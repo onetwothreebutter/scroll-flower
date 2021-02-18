82aa7e3b1db9444d3ffd43fa84dc59b122d99c78
Eric Johnson <ejohnson@csod.com>
2021-02-18 11:47:10 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+/*
+ * main.js
+ */
+
+// Load up the web worker
+const ImageLoaderWorker = new Worker('js/workers/image-loader.worker.js')
+
+
+ImageLoaderWorker.addEventListener('message', event => {
+    // Grab the message data from the event
+    const imageData = event.data
+
+    // Get the original element for this image
+    const imageElement = document.querySelectorAll(`img[data-original-src='${imageData.imageURL}']`)[0];
+    // We can use the `Blob` as an image source! We just need to convert it
+    // to an object URL first
+    const objectURL = URL.createObjectURL(imageData.blob)
+
+    // Once the image is loaded, we'll want to do some extra cleanup
+    imageElement.onload = () => {
+        // Let's remove the original `data-src` attribute to make sure we don't
+        // accidentally pass this image to the worker again in the future
+        imageElement.removeAttribute('data-original-src')
+
+        // We'll also revoke the object URL now that it's been used to prevent the
+        // browser from maintaining unnecessary references
+        URL.revokeObjectURL(objectURL)
+    }
+
+    imageElement.setAttribute('src', objectURL)
+});
+
+const TOTAL_IMAGES = 1410;
+const ACTIVE_CLASS = 'active';
+
+const imageURL = (index) => {
+    const baseURL = `/images/Daffodil-34826${index.toString().padStart(4, '0')}.jpg`;
+    return (window.location.href.includes('localhost')) ?
+        `/scroll-flower${baseURL}` : baseURL;
+}
+
+const addEmptyImgToPage = index => {
+    const newImg = document.createElement('img');
+    newImg.dataset.imageIndex = index;
+    newImg.dataset.originalSrc = imageURL(index);
+    if(index === 1) {
+        newImg.classList.add(ACTIVE_CLASS);
+    }
+    document.body.appendChild(newImg);
+}
+
+const loadImageAtIndex = index => {
+    ImageLoaderWorker.postMessage(imageURL(index));
+};
+
+
+
+for(let i = 0; i <= TOTAL_IMAGES; i += 1) {
+    addEmptyImgToPage(i);
+    loadImageAtIndex(i);
+}
+
+
+const updateImage = (index) => {
+    const activeImage = document.querySelector('.active');
+    const imageToUpdate = document.querySelector(`[data-image-index="${index}"]`);
+    activeImage.classList.remove(ACTIVE_CLASS);
+    imageToUpdate.classList.add(ACTIVE_CLASS);
+}
+
+const frameCount = TOTAL_IMAGES;
+window.addEventListener('scroll', () => {
+    const html = document.querySelector('html');
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(
+        frameCount - 1,
+        Math.ceil(scrollFraction * frameCount)
+    );
+    console.log( 'frameIndex', frameIndex);
+
+    requestAnimationFrame(() => updateImage(frameIndex));
+});
+
---
+++ js/workers/image-loader.worker.js
@@ -0,0 +1,16 @@
+/*
+ * image-loader.worker.js
+ */
+
+self.addEventListener('message', async event => {
+    const imageURL = event.data
+
+    const response = await fetch(imageURL)
+    const blob = await response.blob()
+
+    // Send the image data to the UI thread!
+    self.postMessage({
+        imageURL: imageURL,
+        blob: blob,
+    })
+})

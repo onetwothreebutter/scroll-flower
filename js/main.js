b10459689900dc968d495d69c2b26ac2a0e65f7c
Eric Johnson <ejohnson@csod.com>
2021-02-23 20:42:44 -0600

    Only load every 5th image

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const currentFrame = index => {
 }
 
 const preloadImages = () => {
-    for (let i = 1; i < frameCount; i++) {
+    for (let i = 1; i < frameCount; i += 5) {
         const img = new Image();
         img.src = currentFrame(i);
     }

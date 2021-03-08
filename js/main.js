1eaeeff0bffdd4cd74105f641b885b400b11c09f
Eric Johnson <ejohnson@csod.com>
2021-03-08 15:53:37 -0600

    try setInterval with 0 seconds

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ const imageLoaderFunction = ()=> {
     }
     upperBound = i + 200;
 };
-const imageLoaderInterval = setInterval(imageLoaderFunction, 10000);
+const imageLoaderInterval = setInterval(imageLoaderFunction, 0);
 
 
 

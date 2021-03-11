dc4b37976a29f408628f61fec922d3c8dbd561f6
Eric Johnson <ejohnson@csod.com>
2021-03-11 16:20:06 -0600

    Fix bug with error handling

--- js/workers/image-loader.worker.js
+++ js/workers/image-loader.worker.js
@@ -8,16 +8,15 @@ self.addEventListener('message', async event => {
     try{
         const response = await fetch(imageURL)
         const blob = await response.blob()
+        // Send the image data to the UI thread!
+        self.postMessage({
+            imageURL: imageURL,
+            blob: blob,
+        })
     } catch(e) {
         self.postMessage({
             error: e,
         })
     }
 
-
-    // Send the image data to the UI thread!
-    self.postMessage({
-        imageURL: imageURL,
-        blob: blob,
-    })
 })
